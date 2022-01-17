a4039cd068396d7c3b097ab3e2febf94b58cdcad
Teemu H <swd1tn002@users.noreply.github.com>
2022-01-17 10:50:48 +0200

    Lisätty Maalaus-esimerkki

---
+++ src/viikko01/perusteet/Maalaus.java
@@ -0,0 +1,23 @@
+package viikko01.perusteet;
+
+public class Maalaus {
+
+	public static void main(String[] args) {
+		System.out.println("Anna huoneen leveys, pituus ja korkeus: ");
+		
+		double leveys = 3.40;
+		double pituus = 3.0;
+		double korkeus = 2.5;
+		
+		double nelioitaLitralla = 5.0;
+		
+		double seina1 = leveys * korkeus;
+		double seina2 = pituus * korkeus;
+		
+		double pintaAla = seina1 * 2 + seina2 * 2;
+		
+		double maalinKulutus = pintaAla / nelioitaLitralla;
+		
+		System.out.println("Maalin tarve on " + maalinKulutus + " litraa");
+	}
+}
